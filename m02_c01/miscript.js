09468706ee8ce765e43c4bb3bf39e51747133b20
denisse Rosental <denisserosentalc@gmail.com>
2022-05-30 12:45:09 -0400

    prompt y alert

---
+++ m02_c01/miscript.js
@@ -0,0 +1,9 @@
+//Ejercicio en clase 1
+const nombre = prompt("Ingresa tu nombre: ");
+const bienvenida = alert("Bienvenido: " + nombre);
+const edad = prompt("Ingresa tu edad: ");
+const mensaje = console.log("La edad que ingresaste fue: " + edad);
+
+// Ejercicio en clase 2
+const texto = prompt("Ingresa un texto: ");
+console.log("El texto tiene un total de: " + texto.length + " caracteres");
